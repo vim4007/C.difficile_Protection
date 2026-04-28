4e25dde2b01232a9dd9fcdd9afb05daad8437e15
Vishwas Mishra <107886793+vim4007@users.noreply.github.com>
2026-04-28 13:37:43 -0400

    Update gcms_figures.m

--- analysis/fig6/gcms_figures.m
+++ analysis/fig6/gcms_figures.m
@@ -1,10 +1,5 @@
-% ext_file = '/Users/vmishra/C.difficile_Protection/data/GCMS/secretome.xlsx';
-% int_file = '/Users/vmishra/C.difficile_Protection/data/GCMS/intra.xlsx';
-
-% ext_table = readtable(ext_file, 'Sheet', 1, 'VariableNamingRule', 'preserve');
-% int_table = readtable(int_file, 'Sheet', 1, 'VariableNamingRule', 'preserve');
-
-base_dir = fileparts(fileparts(mfilename('fullpath')));
+script_dir = fileparts(which('gcms_figures'));
+base_dir = fileparts(fileparts(script_dir));
 ext_file = fullfile(base_dir, 'data', 'GCMS', 'secretome.xlsx');
 int_file = fullfile(base_dir, 'data', 'GCMS', 'intra.xlsx');
 ext_table = readtable(ext_file, 'Sheet', 1, 'VariableNamingRule', 'preserve');
